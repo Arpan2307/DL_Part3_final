196d455a474a58b3ca1df1b6c14e7aabb379c345
Arpan Jain <132807088+Arpan2307@users.noreply.github.com>
2025-04-15 16:03:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import json
 import argparse
-import sys
 import logging
-import copy
-import torch
-from utils import factory
-from utils.data_manager import DataManager
-from utils.toolkit import count_parameters
-import os
+import matplotlib.pyplot as plt
 from trainer import train
+
 
 def main():
     args = setup_parser().parse_args()
     param = load_json(args.config)
-    args = vars(args)  # Converting argparse Namespace to a dict.
-    args.update(param)  # Add parameters from json
+    args = vars(args)
+    args.update(param)
 
-    # Run temperature experiments
     temperatures = [0.08, 0.2, 0.7]
     results = run_temperature_experiments(args, temperatures)
-    plot_temperature_vs_forgetting(results)  # Plot the results of the experiments
+    plot_temperature_vs_forgetting(results)
 
 
 def load_json(settings_path):
@@ -29,8 +23,9 @@ def load_json(settings_path):
 
 
 def setup_parser():
-    parser = argparse.ArgumentParser(description='Reproduce of multiple continual learning algorithms.')
-    parser.add_argument('--config', type=str, default='./exps/finetune.json', help='Json file of settings.')
+    parser = argparse.ArgumentParser(description='Run contrastive learning temperature experiments.')
+    parser.add_argument('--config', type=str, default='./exps/finetune.json',
+                        help='Json file of settings.')
     return parser
 
 
@@ -38,25 +33,25 @@ def run_temperature_experiments(args, temperatures):
     results = []
     for temp in temperatures:
         args["contrast_T"] = temp
-        logging.info(f"Running experiment with temperature: {temp}")
-        avg_forgetting = train(args)  # Now this returns avg_forgetting
+        logging.info(f"\nRunning experiment with temperature: {temp}")
+        avg_forgetting = train(args)
+        logging.info(f"Temperature: {temp}, Average Forgetting: {avg_forgetting:.2f}")
         results.append((temp, avg_forgetting))
     return results
 
 
-# Function to plot temperature vs. average forgetting.
 def plot_temperature_vs_forgetting(results):
-    import matplotlib.pyplot as plt
-
     temperatures, forgetting = zip(*results)
-    plt.plot(temperatures, forgetting, marker='o')
+    plt.figure(figsize=(8, 5))
+    plt.plot(temperatures, forgetting, marker='o', linestyle='-', color='b')
     plt.title('Temperature vs. Average Forgetting')
-    plt.xlabel('Temperature')
+    plt.xlabel('Contrastive Temperature')
     plt.ylabel('Average Forgetting')
     plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("temperature_vs_forgetting.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
